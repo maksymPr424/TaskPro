61df0d5309d9583d64508637220d26f92a9cb63b
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-10-30 21:30:45 +0100

    modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+import "modern-normalize/modern-normalize.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>

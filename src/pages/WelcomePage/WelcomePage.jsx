42f4a35830095dddfa626ac8affd87dc2f1718fa
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-11-05 10:45:21 +0100

    path in Welcome page

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,6 +1,6 @@
 import user_image from "../../assets/welcome_page_user_image.png";
 import styles from "./WelcomePage.module.css";
-import svgSprite from "../../assets/sprite.svg";
+import svgSprite from "../../../public/sprite.svg";
 import { Link } from "react-router-dom";
 
 export default function WelcomePage() {

fa3da0a3ace42fa1fa5a2792745292e0a73bb23e
cel3ntano <adriano2024celentano@gmail.com>
2024-11-06 23:31:34 +0100

    Styled error messages and loader for better alignment and consistent placement under the register/login forms

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,19 +3,41 @@ import RegisterForm from "../../components/RegisterForm/RegisterForm.jsx";
 import LoginForm from "../../components/LoginForm/LoginForm.jsx";
 import NotFoundPage from "../NotFound/NotFoundPage.jsx";
 import styles from "./AuthPage.module.css";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { clearError } from "../../redux/auth/slice.js";
 
 export default function AuthPage() {
+  const [formHeight, setFormHeight] = useState(0);
+  const formRef = useRef(null);
   const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(clearError());
+  }, [id, dispatch]);
+
+  useEffect(() => {
+    if (formRef.current) {
+      setFormHeight(formRef.current.offsetHeight);
+    }
+  }, [id]);
 
   return (
     <div className={styles.authPage}>
-      {id === "register" ? (
-        <RegisterForm />
-      ) : id === "login" ? (
-        <LoginForm />
-      ) : (
-        <NotFoundPage />
-      )}
+      <div
+        className={styles.formContainer}
+        style={{ height: `${formHeight}px` }}>
+        <div ref={formRef}>
+          {id === "register" ? (
+            <RegisterForm />
+          ) : id === "login" ? (
+            <LoginForm />
+          ) : (
+            <NotFoundPage />
+          )}
+        </div>
+      </div>
     </div>
   );
 }

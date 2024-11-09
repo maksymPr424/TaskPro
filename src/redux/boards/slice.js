b5a2aa20d9c72562c18194ade5e47021edfa4bdd
cel3ntano <adriano2024celentano@gmail.com>
2024-11-09 05:30:54 +0100

    Add logic to retrieve boards and last active board during login and refresh

--- src/redux/boards/slice.js
+++ src/redux/boards/slice.js
@@ -1,22 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchBoards, removeBoard } from './operations';
+import { fetchBoards, removeBoard } from "./operations";
 
 const boardsSlice = createSlice({
-  name: 'boards',
+  name: "boards",
   initialState: {
+    lastActiveBoard: null,
     boards: [],
     loading: false,
     error: null,
   },
   reducers: {
     setBoards: (state, action) => {
-      state.boards = action.payload;
+      state.lastActiveBoard = action.payload.lastActiveBoard;
+      state.boards = action.payload.boards;
     },
     addBoard: (state, action) => {
       state.boards.push(action.payload);
     },
     updateBoard: (state, action) => {
-      const index = state.boards.findIndex(board => board._id === action.payload._id);
+      const index = state.boards.findIndex(
+        board => board._id === action.payload._id
+      );
       if (index !== -1) {
         state.boards[index] = { ...state.boards[index], ...action.payload };
       }
@@ -24,18 +28,20 @@ const boardsSlice = createSlice({
     deleteBoard: (state, action) => {
       state.boards = state.boards.filter(board => board._id !== action.payload);
     },
-    clearBoards: (state) => {
+    clearBoards: state => {
       state.boards = [];
     },
   },
 
-  extraReducers: (builder) => {
+  extraReducers: builder => {
     builder
       .addCase(fetchBoards.fulfilled, (state, action) => {
         state.boards = action.payload;
       })
       .addCase(removeBoard.fulfilled, (state, action) => {
-        state.boards = state.boards.filter(board => board._id !== action.payload);
+        state.boards = state.boards.filter(
+          board => board._id !== action.payload
+        );
       })
       .addCase(removeBoard.rejected, (state, action) => {
         state.error = action.payload;
@@ -43,5 +49,6 @@ const boardsSlice = createSlice({
   },
 });
 
-export const { setBoards, addBoard, updateBoard, deleteBoard, clearBoards } = boardsSlice.actions;
+export const { setBoards, addBoard, updateBoard, deleteBoard, clearBoards } =
+  boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;

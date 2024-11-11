d34eeaef8b83dedf45194c134949037c5674f983
cel3ntano <adriano2024celentano@gmail.com>
2024-11-11 16:09:48 +0100

    fix header and sidebar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
-import Header from "../../components/Header/Header";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import ScreensPage from "../ScreensPage/ScreensPage.jsx";
-import styles from "./HomePage.module.css";
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import ScreensPage from '../ScreensPage/ScreensPage.jsx';
+import styles from './HomePage.module.css';
 
 export default function HomePage() {
   return (
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,14 +1,10 @@
-
-import MainDashboard from "../../components/MainDashboard/MainDashboard";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import css from "./ScreensPage.module.css";
+import MainDashboard from '../../components/MainDashboard/MainDashboard';
+import css from './ScreensPage.module.css';
 
 export default function ScreensPage() {
   return (
     <>
-      <div className={css.sidebar}>
-        <Sidebar />
-      </div>
       <MainDashboard />
     </>
-  )}
+  );
+}
